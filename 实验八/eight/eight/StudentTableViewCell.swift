2abfad9d09591e1a829798f945bd2d0ceee0bd9d
Moistendust <45845617+Moistendust@users.noreply.github.com>
2018-12-18 11:13:24 +0800

    Add files via upload

---
+++ 实验八/eight/eight/StudentTableViewCell.swift
@@ -0,0 +1,27 @@
+//
+//  StudentTableViewCell.swift
+//  eight
+//
+//  Created by student on 2018/12/17.
+//  Copyright © 2018年 futing. All rights reserved.
+//
+
+import UIKit
+
+class StudentTableViewCell: UITableViewCell {
+    
+    @IBOutlet weak var name: UILabel!
+    @IBOutlet weak var age: UILabel!
+    @IBOutlet weak var gender: UILabel!
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+    
+    override func setSelected(_ selected: Bool, animated: Bool) {
+        super.setSelected(selected, animated: animated)
+        
+        // Configure the view for the selected state
+    }
+    
+}
